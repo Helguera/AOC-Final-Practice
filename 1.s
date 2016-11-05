1585451be2e88b0da4c70648a4acf8cf7bb0dd27
Javier Helguera <javier.helguera@alumnos.uva.es>
2016-11-05 01:31:31 +0100

    Funcion dias entre fechas mismo año completa
    Pasada toda la bateria de prueba para fechas dentro del mismo año (sin bisiesto)

--- 1.s
+++ 1.s
@@ -4,12 +4,12 @@ temp:	.word 	0
 	.globl __start
 	.text
 __start:
-	add $s0,$zero, 4
-	add $s1,$zero, 3
+	add $s0,$zero, 17
+	add $s1,$zero, 8
 	add $s2,$zero, 2015
 
-	add $s3,$zero, 3
-	add $s4,$zero, 3
+	add $s3,$zero, 2
+	add $s4,$zero, 4
 	add $s5,$zero, 2015
 	
 	jal calculo	
@@ -25,30 +25,33 @@ __start:
 
 	
 #------------------------------------------------------------
-calculo:slt $t0, $s1, $s4
-	bne $t0, 0 , sigue
+calculo:sub $t1, $s1, $s4
+	slt $t2, $s1, $s4
+	beq $t1,0, comprueba_dia
+	beq $t2,0,cambia_todo
 	
+comprueba_dia:
+	slt $t1, $s0, $s3
+	beq $t1, 1, inicio
+	move $t1, $s0
+	move $s0, $s3
+	move $s3, $t1
+	j inicio
+
+cambia_todo:
 	la $a0, temp
-	sw $s0, 0($a0)
+	sw $s0,0($a0)
 	sw $s1,4($a0)
 	move $s0,$s3
-	move $s1,$s4
+	move $s1, $s4
 	lw $s3,0($a0)
 	lw $s4,4($a0)
 	
-sigue:	slt $t0,$s4,$s1
-	bne $t0,1, sigue2
-	slt $t0,$s0,$s3
-	beq $t0,1,sigue2
-	move $t0,$s0
-	move $s0,$s4
-	move $s4,$t0
-	
-sigue2:	la $a0, dias
-	add $s6,$zero,$s1
-	add $s6,$s6,-1
-	add $s7,$zero,0
+inicio:	la $a0,dias
 	add $a0,$a0,$s1
+	add $s7,$zero,0
+	move $s6,$s1
+	add $s6, $s6,-1
 	
 bucle:	lb $t0,0($a0)
 	add $s7,$s7,$t0
@@ -63,9 +66,6 @@ bucle:	lb $t0,0($a0)
 	sub $t0, $t0,$s3
 	sub $s7, $s7, $t0
 	
-	sub $t0, $s1,$s4
-	bne $t0,0, salir
-	sub $s7,$zero,$s7
 salir:	jr $ra
 #---------------------------------------------
 bisiesto:la $a0, dias
